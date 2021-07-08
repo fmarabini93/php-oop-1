7f5cf55d15f1dc1402a9fda0ac2eb7f4608ce7c5
fmarabini93 <fmarabini@icloud.com>
2021-07-08 16:14:30 +0200

    films_db

---
+++ database.php
@@ -0,0 +1,46 @@
+<?php
+    $films = [
+        [
+            "title" => "Wonder Woman 1984",
+            "overview" => "A botched store robbery places Wonder Woman in a global battle against a powerful and mysterious ancient force that puts her powers in jeopardy.",
+            "original_lang" => "English",
+            "genre" => "Fantasy, Action, Adventure",
+            "duration" => "2h 31m"
+        ],
+        [
+            "title" => "Luca",
+            "overview" => "Luca and his best friend Alberto experience an unforgettable summer on the Italian Riviera. But all the fun is threatened by a deeply-held secret: they are sea monsters from another world just below the water’s surface.",
+            "original_lang" => "Italian",
+            "genre" => "Animation, Comedy, Family, Fantasy",
+            "duration" => "1h 35m"
+        ],
+        [
+            "title" => "Mortal Kombat",
+            "overview" => "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.",
+            "original_lang" => "English",
+            "genre" => "Action, Fantasy, Adventure",
+            "duration" => "1h 50m"
+        ],
+        [
+            "title" => "The Simpsons",
+            "overview" => "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
+            "original_lang" => "English",
+            "genre" => "Family, Animation, Comedy, Drama",
+            "duration" => "22m"
+        ],
+        [
+            "title" => "Lucifer",
+            "overview" => "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.",
+            "original_lang" => "English",
+            "genre" => "Crime, Sci-Fi & Fantasy",
+            "duration" => "45m"
+        ],
+        [
+            "title" => "Need for Speed",
+            "overview" => "The film revolves around a local street-racer who partners with a rich and arrogant business associate, only to find himself framed by his colleague and sent to prison. After he gets out, he joins a New York-to-Los Angeles race to get revenge. But when the ex-partner learns of the scheme, he puts a massive bounty on the racer's head, forcing him to run a cross-country gauntlet of illegal racers in all manner of supercharged vehicles.",
+            "original_lang" => "English",
+            "genre" => "Action, Crime, Drama, Thriller",
+            "duration" => "2h 12m"
+        ]   
+    ];
+?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    include 'database.php';
     class Movie {
         public $title;
         public $overview;
@@ -20,12 +21,12 @@
         }
     }
     
-    $wonder_woman = new Movie("Wonder Woman 1984", "A botched store robbery places Wonder Woman in a global battle against a powerful and mysterious ancient force that puts her powers in jeopardy.", "English", " Fantasy, Action, Adventure", "2h 31m");
-    $luca = new Movie("Luca", "Luca and his best friend Alberto experience an unforgettable summer on the Italian Riviera. But all the fun is threatened by a deeply-held secret: they are sea monsters from another world just below the water’s surface.", "Italian", " Animation, Comedy, Family, Fantasy", "1h 35m");
-    $mortal_kombat = new Movie("Mortal Kombat", "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.", "English", "Action, Fantasy, Adventure", "1h 50m");
-    $the_simpsons = new Movie("The Simpsons", "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.", "English", "Family, Animation, Comedy, Drama", "22m");
-    $lucifer = new Movie("Lucifer", "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.", "English", "Crime, Sci-Fi & Fantasy", "45m");
-    $need_for_speed = new Movie("Need for Speed", "The film revolves around a local street-racer who partners with a rich and arrogant business associate, only to find himself framed by his colleague and sent to prison. After he gets out, he joins a New York-to-Los Angeles race to get revenge. But when the ex-partner learns of the scheme, he puts a massive bounty on the racer's head, forcing him to run a cross-country gauntlet of illegal racers in all manner of supercharged vehicles.", "English", "Action, Crime, Drama, Thriller", "2h 12m");
+    $wonder_woman = new Movie($films[0]["title"], $films[0]["overview"], $films[0]["original_lang"], $films[0]["genre"], $films[0]["duration"]);
+    $luca = new Movie($films[1]["title"], $films[1]["overview"], $films[1]["original_lang"], $films[1]["genre"], $films[1]["duration"]);
+    $mortal_kombat = new Movie($films[2]["title"], $films[2]["overview"], $films[2]["original_lang"], $films[2]["genre"], $films[2]["duration"]);
+    $the_simpsons = new Movie($films[3]["title"], $films[3]["overview"], $films[3]["original_lang"], $films[3]["genre"], $films[3]["duration"]);
+    $lucifer = new Movie($films[4]["title"], $films[4]["overview"], $films[4]["original_lang"], $films[4]["genre"], $films[4]["duration"]);
+    $need_for_speed = new Movie($films[5]["title"], $films[5]["overview"], $films[5]["original_lang"], $films[5]["genre"], $films[5]["duration"]);
 ?>
 
 <!DOCTYPE html>
